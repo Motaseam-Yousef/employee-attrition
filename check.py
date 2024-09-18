68c4b7eed90d750842028b04cc95d34553e620fd
Motaseam Yousef <motaseamsaher@gmailcom>
2024-09-18 10:41:33 +0300

    add lib version

---
+++ check.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import matplotlib
+import sklearn
+import flask
+import joblib
+import imblearn
+import xgboost
+
+# Print the versions
+print(f"pandas: {pd.__version__}")
+print(f"numpy: {np.__version__}")
+print(f"matplotlib: {matplotlib.__version__}")
+print(f"scikit-learn: {sklearn.__version__}")
+print(f"Flask: {flask.__version__}")
+print(f"joblib: {joblib.__version__}")
+print(f"imbalanced-learn: {imblearn.__version__}")
+print(f"xgboost: {xgboost.__version__}")
